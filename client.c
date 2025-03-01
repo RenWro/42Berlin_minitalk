b3a8b008aa7e97b4d638fcf7fc34263b8ba04e27
Ren Wro <134458911+RenWro@users.noreply.github.com>
2025-03-01 12:41:27 +0100

    Update client.c
    Update client to include server response, deal with pause, added global variable.

--- client.c
+++ client.c
@@ -6,12 +6,26 @@
 /*   By: rwrobles <rwrobles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 16:11:47 by rwrobles          #+#    #+#             */
-/*   Updated: 2025/02/28 17:21:26 by rwrobles         ###   ########.fr       */
+/*   Updated: 2025/03/01 17:21:26 by rwrobles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <sys/types.h> 
+
+/* 
+ * Global variable to track server acknowledgment.
+ */
+static int g_reply = 0;
+
+/* 
+ * Signal handler to update the global reply variable when a SIGUSR1 is received.
+ */
+void	signal_reception(int signal)
+{
+	if (signal == SIGUSR1)
+		g_reply = 1;
+}
 
 /* 
  * Function to send a single bit to the server via a signal.
@@ -20,25 +34,16 @@
  * process the signal.
  */
 
-/*
- * The `pid_t` (Process ID Type) is a data type defined in the POSIX standard 
- *library (`sys/types.h`) that represents a process identifier (Process ID or 
- * simply PID). 
- * It is used to store values that identify processes in the operating system.
- *
- * It is a signed integer type, usually represented as `int`, but the exact 
- *  implementation may vary between operating systems.
- * It is used to identify processes in system calls such as `fork()`, `kill()`, 
- * `wait()`, and `getpid()`.
- */
-
 void	send_bit_to_server(pid_t server_pid, int bit_value)
 {
 	if (bit_value == 1)
 		kill(server_pid, SIGUSR1);
 	else
 		kill(server_pid, SIGUSR2);
-	usleep(42);
+	usleep(10000);
+	while (!g_reply)
+		pause();
+	g_reply = 0;
 }
 
 /* 
@@ -52,23 +57,21 @@ void	send_message_to_server(pid_t server_pid, const char *message)
 {
 	unsigned char	current_char;
 	int				bit_position;
+	size_t			i;
+	size_t			len;
 
-	while (*message != '\0')
+	len = ft_strlen(message) + 1;
+	i = 0;
+	while (i < len)
 	{
-		current_char = *message;
-		bit_position = 7; 
+		current_char = message[i];
+		bit_position = 7;
 		while (bit_position >= 0)
 		{
 			send_bit_to_server(server_pid, (current_char >> bit_position) & 1);
 			bit_position--;
 		}
-		message++;
-	}
-	bit_position = 7;
-	while (bit_position >= 0)
-	{
-		send_bit_to_server(server_pid, 0);
-		bit_position--;
+		i++;
 	}
 }
 
@@ -85,9 +88,15 @@ int	main(int argc, char **argv)
 	if (argc != 3)
 	{
 		ft_putendl_fd("Usage: <server PID> <message>", 1);
-		exit(1);
+		return (1);
 	}
 	server_pid = ft_atoi(argv[1]);
+	if (server_pid <= 0)
+	{
+		ft_putendl_fd("Error: Invalid PID.", 1);
+		return (1);
+	}
+	signal(SIGUSR1, signal_reception);
 	message = argv[2];
 	send_message_to_server(server_pid, message);
 	return (0);
