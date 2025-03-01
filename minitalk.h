290f4dfb1fff8a8ac365a7ada9f2c397405a2a55
Ren Wro <134458911+RenWro@users.noreply.github.com>
2025-03-01 13:19:43 +0100

    Update minitalk.h
    Added struct

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: rwrobles <rwrobles@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 15:53:24 by rwrobles          #+#    #+#             */
-/*   Updated: 2025/02/28 15:53:39 by rwrobles         ###   ########.fr       */
+/*   Updated: 2025/03/02 11:53:39 by rwrobles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,16 @@
 
 # define BUFFER 90000
 
+/* 
+ * Struct to manage dynamic message storage.
+ * - `char *msg`: Holds the dynamically allocated message.
+ * - `int current_char`: Stores the character being reconstructed from bits.
+ */
+typedef struct s_message
+{
+	char	*msg;
+	int		current_char;
+}	t_message;
+
 #endif
+
